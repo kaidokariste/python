59bae9b6cd7eb9baa0d54725325cb399fcfcc2a8
kaido.kariste <kaido.kariste@bigbank.ee>
2021-11-15 21:54:24 +0200

    Direct exchange

--- RabbitMq/weather-provider.py
+++ RabbitMq/weather-provider.py
@@ -5,38 +5,47 @@ import time
 
 
 def ilm():
-    towns = ['Tallinn','Tartu','Narva','Valga','Kuressaare']
-    random_town= towns[randrange(len(towns))]
-    random_temp = randrange(-15,30,1)
+    towns = ['Tallinn', 'Tartu', 'Narva', 'Valga', 'Kuressaare']
+    random_town = towns[randrange(len(towns))]
+    random_temp = randrange(-15, 30, 1)
     weather_in_town = {"TownName": random_town, "Temperature": random_temp}
     json_weather_in_town = json.dumps(weather_in_town)
     return json_weather_in_town
 
+
 def send_to_Rabbit():
     weather_forecast = ilm()
+    season = 'winter'
+    soojus = json.loads(weather_forecast)
+    if soojus['Temperature'] > 0:
+        season = 'summer'
+
     # By using amqps:// you will hit certification validation errors, so use amqp:// instead
-    url = 'amqp://<url>'
+    url = 'amqp://<cloud-rabbit-address>'
     parameters = pika.URLParameters(url)
 
     connection = pika.BlockingConnection(parameters)
     channel = connection.channel()
 
-    channel.exchange_declare(exchange='estonia_weather', exchange_type='fanout')
+    channel.exchange_declare(exchange='estonia_weather', exchange_type='direct')
 
     channel.basic_publish(exchange='estonia_weather',
-                          routing_key='',
+                          routing_key=season,
                           body=weather_forecast)
+
     print(" [x] Sent " + weather_forecast)
     connection.close()
+
 
 def messagesender():
     i = 0
     # Send new message after every 5 seconds
     while i < 5:
         send_to_Rabbit()
-        i+=1
+        i += 1
         print(i)
         time.sleep(1)
+
 
 if __name__ == "__main__":
     messagesender()
