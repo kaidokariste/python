0c77fb371a6500c69b95f50df4ed4ba8967574dd
kaido.kariste <kaido.kariste@bigbank.ee>
2021-12-02 23:30:45 +0200

    ERR uudiste jälgija

---
+++ Requests/err_uudised.py
@@ -0,0 +1,35 @@
+import requests
+from bs4 import BeautifulSoup
+import dateutil.parser
+
+dates = []
+f = open("newsbenchmark.txt", "r")
+currentbenchmark = dateutil.parser.parse(f.read())
+print('Failist saadu', currentbenchmark)
+
+r = requests.get("https://www.err.ee/rss")
+soup = BeautifulSoup(r.content, features='lxml-xml')
+articles = soup.findAll('item')
+
+
+for a in articles:
+    pubDate = a.find('pubDate').text
+    isodate = dateutil.parser.parse(pubDate)
+
+    if currentbenchmark < isodate:
+        title = a.find('title').text
+        description = a.find('description').text
+
+        #Preparing fleep message
+        message = '*{}*\n{}\n{}'.format(title,isodate.strftime("%H:%M %d.%m.%Y"),description)
+        requests.post("<fleep-web-hook>", json={"message": message, "user": "DWH News Agency"})
+        all_news_dates = dates.append(isodate)
+
+try:
+    currentbenchmark = max(dates)
+except ValueError:
+    currentbenchmark
+
+f = open("newsbenchmark.txt", "w")
+f.write(currentbenchmark.strftime("%Y-%m-%d %H:%M:%S%z"))
+f.close()
