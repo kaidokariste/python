fef5fe297284cff4c0c05bcbbb20b5ded7b20563
kaido.kariste <kaido.kariste@bigbank.ee>
2021-12-06 21:12:16 +0200

    V1.2 Exclude some topics

--- Requests/err_uudised.py
+++ Requests/err_uudised.py
@@ -3,9 +3,13 @@ from bs4 import BeautifulSoup
 import dateutil.parser
 
 dates = []
+# List of excluded topics
+exclude_topics = ['Arhitektuur', 'Teater', 'ETV uudised', 'Kunst', 'Raadiouudised', 'Välismaa', 'Inimesed',
+                  'ETV spordi lühiuudised','Arvamus']
+
 f = open("newsbenchmark.txt", "r")
 currentbenchmark = dateutil.parser.parse(f.read())
-print('Failist saadu', currentbenchmark)
+# print('Initial benchmark', currentbenchmark)
 
 r = requests.get("https://www.err.ee/rss")
 soup = BeautifulSoup(r.content, features='lxml-xml')
@@ -15,14 +19,14 @@ articles = soup.findAll('item')
 for a in articles:
     pubDate = a.find('pubDate').text
     isodate = dateutil.parser.parse(pubDate)
-
-    if currentbenchmark < isodate:
+    category = a.find('category').text
+    # Get only news where date is newer than current saved benchmark and category is not excluded
+    if currentbenchmark < isodate and category not in exclude_topics:
         title = a.find('title').text
         description = a.find('description').text
         link = a.find('link').text
-
         #Preparing fleep message
-        message = '*{}<<{}>>*\n`{}`\n{}'.format(link,title,isodate.strftime("%H:%M %d.%m.%Y"),description)
+        message = '*{}<<{}>>*\n`{} | {}`\n{}'.format(link,title,isodate.strftime("%H:%M %d.%m.%Y"),category, description)
         requests.post("<fleep-web-hook>", json={"message": message, "user": "DWH News Agency"})
         all_news_dates = dates.append(isodate)
 
