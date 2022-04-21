17ed66e8867107b39595c278c831ddccc4988413
kaido.kariste <kaido.kariste@bigbank.ee>
2022-04-21 18:06:57 +0300

    Äripäeva projekt

--- Requests/aripaev.py
+++ Requests/aripaev.py
@@ -1,48 +1,42 @@
 import requests
 from bs4 import BeautifulSoup
 import dateutil.parser
-import ilmateade as wt
-import aripaev as apv
 
-def err_news():
+
+def aripaev_news():
     dates = []
     # List of excluded topics
-    exclude_topics = ['Arhitektuur', 'Teater', 'ETV uudised', 'Kunst', 'Raadiouudised', 'Välismaa', 'Inimesed',
-                  'ETV spordi lühiuudised','Arvamus']
+    exclude_topics = []
 
-    f = open("newsbenchmark.txt", "r")
+    f = open("aripaevbenchmark.txt", "r")
     currentbenchmark = dateutil.parser.parse(f.read())
-    # print('Initial benchmark', currentbenchmark)
+    #print('Initial benchmark', currentbenchmark)
 
-    r = requests.get("https://www.err.ee/rss")
+    r = requests.get("http://feeds.feedburner.com/aripaev-rss")
     soup = BeautifulSoup(r.content, features='lxml-xml')
     articles = soup.findAll('item')
 
-
     for a in articles:
-        pubDate = a.find('pubDate').text
+        pubDate = a.find('pubDate').text.strip()
         isodate = dateutil.parser.parse(pubDate)
-        category = a.find('category').text
+        category = a.find('category').text.strip()
         # Get only news where date is newer than current saved benchmark and category is not excluded
         if currentbenchmark < isodate and category not in exclude_topics:
-            title = a.find('title').text
-            description = a.find('description').text
-            link = a.find('link').text
+            title = a.find('title').text.strip()
+            description = a.find('description').text.strip()
+            link = a.find('link').text.strip()
             #Preparing fleep message
-            message = '*{}<<{}>>*\n`{} | {}`\n{}'.format(link,title,isodate.strftime("%H:%M %d.%m.%Y"),category, description)
+            message = '*{}<<{}>>*\n`{} | {} | Äripäev`\n{}'.format(link,title,isodate.strftime("%H:%M %d.%m.%Y"),category, description)
             requests.post("<fleep-web-hook>", json={"message": message, "user": "DWH News Agency"})
             dates.append(isodate)
-
     try:
         currentbenchmark = max(dates)
     except ValueError:
         pass
 
-    f = open("newsbenchmark.txt", "w")
+    f = open("aripaevbenchmark.txt", "w")
     f.write(currentbenchmark.strftime("%Y-%m-%d %H:%M:%S%z"))
     f.close()
 
 if __name__ == '__main__':
-    err_news()
-    wt.weather_forecast()
-    apv.aripaev_news()
+    aripaev_news()
